b231a415d86235dee36b548bb568f84dbcd86dde
Kechi-kenneth <kaypluzkechi@gmail.com>
2024-07-07 20:03:21 +0100

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
 
-import React from 'react'
-import './App.css'
-import NavBar from './Components/NavBar/NavBar'
+const App: React.FC = () => {
+  const NavBarItems: string[] = [
+    "My feed",
+    "Discussions",
+    "chatter Trend",
+    "More",
+    "Search",
+    "Post",
+    "Darkmood",
+    "Notification",
+    "Profile",
+  ];
+  
 
-
-const App:React.FC = () => {
   return (
     <>
-  <NavBar />
+      <NavBar
+        icon="/public/icons8-favorite-chat-message-100.png"
+        title="Chatter"
+        items={NavBarItems}
+      />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,67 +1,30 @@
-import React from "react";
+
 import "./NavBar.css";
 
-const NavBar: React.FC = () => {
+
+interface NavbarProps {
+  title: string;
+  icon: string; 
+  items: string[]; 
+}
+const NavBar:React.FC <NavbarProps> = ({icon, title, items}) => {
   return (
     <>
       <section>
         <nav className="nav-wrapper">
-          <div className="nav-content">
-        
-            <img
-              className="nav-logo"
-              src="/public/icons8-favorite-chat-message-100.png"
-              alt=""
-            ></img>
-            <p className="app-name" style={{marginLeft:-120}}>Chatter</p>
-          
-           
-            <ul>
-              <li>
-                <a className="menu-item">My feed</a>
-              </li>
-              <li>
-                <a className="menu-item">Discussions</a>
-              </li>
-              <li>
-                <a className="menu-item chatters-trend fade-in-out">Chatters Trend</a>
-              </li>
-              <li>
-                <a className="menu-item">More</a>
-              </li>
-
-              <li>
-                <a className="menu-item">
-                  <img className="search-icon" src="/public/icons8-search-52.png" />
-                </a>
-              </li>
-
-              <li>
-                <a className="menu-item">
-                  <img src="" />
-                  Write..
-                </a>
-              </li>
-              <li>
-                <a className="menu-item">
-                  Darkmode
-                  <img src="" />
-                </a>
-              </li>
-              <li>
-                <a className="menu-item">
-                  Notification
-                  <img src="" />
-                </a>
-              </li>
-              <li>
-                <a className="menu-item">
-                  Profile
-                  <img src="" />
-                </a>
-              </li>
-            </ul>
-          </div>
+         <div className="nav-content">
+        <div>
+          <h1 className="app-name">{title}</h1>
+          <img className="nav-logo" src={icon} />
+        </div>
+        <ul className="menu-item">
+        {items.map((item, index) => (
+          <li key={index}>
+            {item}
+          </li>
+        ))}
+        </ul>
+        </div>
         </nav>
       </section>
     </>
