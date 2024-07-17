01f05c6118401614649aa3ae5218099d9045fe3e
Kechi-kenneth <kaypluzkechi@gmail.com>
2024-07-17 09:08:59 +0100

    ready for mainpage

--- src/Pages/SignIn/SignInPage.tsx
+++ src/Pages/SignIn/SignInPage.tsx
@@ -7,7 +7,7 @@ const SignInPage: React.FC = () => {
 
   const handleToggleButtonClick = () => {
     setIsFormVisible(!isFormVisble);
-  }
+  };
   const navigate = useNavigate();
   const handleSignIn = (event: React.FormEvent) => {
     event.preventDefault();
@@ -18,6 +18,11 @@ const SignInPage: React.FC = () => {
 
   return (
     <div className="container">
+      <Link to="/">
+        <p className="back-symbol">
+          <img src="icons8-left-arrow-50.png" alt="" />
+        </p>
+      </Link>
       <h1 className="sign-in-title">Sign In</h1>
       <div className="social-buttons">
         <button>
@@ -38,28 +43,46 @@ const SignInPage: React.FC = () => {
           <span>
             <img src="" alt="" />
           </span>{" "}
-          Login with Email
+          Login with Email or Number
         </button>
         {isFormVisble && (
-            <form className="form" onSubmit={handleSignIn}>
+          <form className="form" onSubmit={handleSignIn}>
             <div>
-              <input type="email" id="email" name="email" placeholder="Email or Number" required />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email or Number"
+                required
+              />
             </div>
             <div>
-              
-              <input type="password" id="password" name="password" placeholder="Password" required />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
             </div>
             <div>
-              <input type="password" id="confirmPassword" placeholder="confirm Password" name="confirmPassword" required />
+              <input
+                type="password"
+                id="confirmPassword"
+                placeholder="confirm Password"
+                name="confirmPassword"
+                required
+              />
             </div>
-            <button className="toggle-signin" type="submit">Sign In</button>
+            <button className="toggle-signin" type="submit">
+              Sign In
+            </button>
           </form>
         )}
       </div>
-    
-      <Link to="/signup"><p>Don't have an account? Sign Up</p></Link>
-      <Link to="/">
-        <p>Back symbol</p>
+      <p className="sign-up">Don't have an Account?</p>
+      <Link to="/welcome">
+        <p className="sign-up">Sign Up </p>
       </Link>
     </div>
   );
